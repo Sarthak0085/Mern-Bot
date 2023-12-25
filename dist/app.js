c06a43f355c3d312b7d8df96ce83049f47352b48
Sarthak0085 <sarth.mahajan@gmail.com>
2023-12-25 22:05:01 +0530

    render commit

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { config } from "dotenv";
-import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -10,13 +9,14 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["mern-bot-client.vercel.app",],
     credentials: true,
-    optionsSuccessStatus: 200,
 }));
 //remove it in production
-app.use(morgan("dev"));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+app.get("/", (req, res) => {
+    res.json("test");
+});
 app.use("/api/v1", appRouter);
 app.use(ErrorMiddleware);
 export default app;
--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -4,7 +4,7 @@ class ErrorHandler extends Error {
         super(message);
         this.statusCode = statusCode;
 
-        Error.captureStackTrace(this, this.constructor);
+        (Error as any).captureStackTrace(this, this.constructor);
     }
 }
 
